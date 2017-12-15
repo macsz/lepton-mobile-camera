a82fee034c50c2f56c945f2559a88cc6eb33fdf6
Maciej Szankin <maciej@szankin.pl>
2017-12-14 20:48:47 -0800

    Added resizing images when converting

--- convert_images.sh
+++ convert_images.sh
@@ -2,5 +2,5 @@
 
 mkdir -p converted
 
-for IMG in `ls 'images/' | grep pgm`; do convert images/$IMG converted/$IMG.jpg; done;
+for IMG in `ls 'images/' | grep pgm`; do convert images/$IMG -resize 800x600 converted/$IMG.jpg; done;
 echo Done.;
